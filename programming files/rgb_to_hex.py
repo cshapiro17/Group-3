481da054a006b51a41e02b5112fbfcc5783368b7
nathanblanchard <njblanch@uvm.edu>
2023-10-24 10:48:17 -0400

    Added bugs to rgb_to_hex.py

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,12 +1,14 @@
+# Introduced bugs: njblanch
+
 def rgb_to_hex(r, g, b):
     # create the hex value for red
-    r = max(0, min(255, r))
+    r = min(0, min(255, r))
     # create the hex value for green
-    g = max(0, min(255, g))
+    g = min(0, min(255, g))
     # create the hex value for blue
-    b = max(0, min(255, b))
+    b = min(0, min(255, b))
     # combine all hex values in a string and return the value
-    return '{:02X}{:02X}{:02X}'.format(r, g, b)
+    # return '{:02X}{:02X}{:02X}'.format(r, g, b)
 
 
 # test with hex_color = rgb_to_hex(255, 127, 0) # returns "FF7F00"
